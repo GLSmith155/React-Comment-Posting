2478b26c3574645906927bacffafa0e815e3496d
George Smith <40671362+GLSmith155@users.noreply.github.com>
2020-12-04 11:02:12 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,51 @@
+// Importing the React and ReactDOM libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CommentDetail from './CommentDetail';
+import faker from 'faker';
+import ApprovalCard from './ApprovalCard';
+
+
+// Creating React Components
+const App = () => {
+    return (
+        <div className="ui contianer comments">
+          <ApprovalCard>
+                <h4>Warning!</h4>
+              Are you sure?
+          </ApprovalCard>
+
+
+          <ApprovalCard>
+            <CommentDetail 
+                author="Shoulder" 
+                timeAgo="Yesterday at 8:45 PM"
+                commentMessage="Nice post!"
+                fakerImage={faker.image.image()} 
+            /> 
+          </ApprovalCard>  
+
+          <ApprovalCard>
+            <CommentDetail 
+                author="Leg" 
+                timeAgo="Today at 6:25 PM"
+                commentMessage="Wow neat"
+                fakerImage={faker.image.image()} 
+            /> 
+          </ApprovalCard>  
+
+          <ApprovalCard>
+            <CommentDetail 
+                author="Arm" 
+                timeAgo="Today at 7:34 PM" 
+                commentMessage="Where is that at?"
+                fakerImage={faker.image.image()} 
+            /> 
+          </ApprovalCard>  
+        </div>
+    );
+};
+
+
+// Take the react component and show it on the screen
+ReactDOM.render(<App />,document.querySelector('#root')); 
